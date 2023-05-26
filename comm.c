7798610ef6d176698d1e4f1c8dd82bc92bdda8c6
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-05-26 14:21:30 -0700

    refactor: fix comm.c to pass betty check

--- comm.c
+++ comm.c
@@ -1,16 +1,24 @@
 #include "shell.h"
 
-void handle_comments(char *line) {
-  int i = 0;
+/**
+ * handle_comments - handles comments passed as arguments
+ * @line: line of code to be checked
+ */
+void handle_comments(char *line)
+{
+	int i = 0;
 
-  while (line[i] != '\0') {
-    if (line[i] == '#') {
-      // This is a comment.
-      i = strlen(line);
-    } else {
-      // This is not a comment.
-      break;
-    }
-  }
+	while (line[i] != '\0')
+	{
+		if (line[i] == '#')
+		{
+			/* This is a comment. */
+			i = strlen(line);
+		}
+		else
+		{
+			/* This is not a comment. */
+			break;
+		}
+	}
 }
-
